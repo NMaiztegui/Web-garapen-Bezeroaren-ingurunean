a474d743605b1879ff09116e895926bae0881bff
NMaiztegui <naroamaiztegui@gmail.com>
2024-09-17 15:39:24 +0200

    irakurritako_libruak funtzioa

--- Erronak1/ARIKETA 4/js/ariketa4.js
+++ Erronak1/ARIKETA 4/js/ariketa4.js
@@ -1,34 +1,42 @@
+let liburuak_list=[]
 function liburuak(titulua,autorea,irakurrita){
-  var liburua = {
+  return  {
    lTitulua : titulua,
    lAutore : autorea,
-   lIrakurrta: irakurrita
-  }
+   lIrakurrita: irakurrita.toLowerCase()==="bai" // guardar como boolean (true o false)
+  };
   
-  return liburua
+  
 }
-function libruak_eskatu(){
+function main(){
    let lib_sartu = prompt('Liburu berria satu nahi dizu? erantzun bai edo ez')
-   let x = 0
+   
    do{
       let titulo= prompt('Sartu liburuaren titulua')
       let autor = prompt('Sartu liburuaren autorea')
-      let irakurri =(prompt('Librua irakurri duzu? Sartu bai edo ez'))
+      let irakurri 
 
-     let librua= liburuak (titulo,autor,irakurri)
-     liburen_lista(librua,x)
+      do{
+         irakurri =(prompt('Librua irakurri duzu? Sartu bai edo ez')).toLowerCase()
+      }while (irakurri!="bai" &&  irakurri!="bai")
+
+      liburuak_list.push(liburuak (titulo,autor,irakurri))
 
       lib_sartu = prompt('Liburu berria satu nahi dizu? erantzun bai edo ez')
-      x++
-   }while(lib_sartu=="bai")
+      
+   }while(lib_sartu.toLowerCase()==="bai")
    
-  
+  irakurritako_liburuak()
 }
 
-function liburen_lista(librua,x){
-   let lista = []
-   lista[x]=librua
-   return lista
+function irakurritako_liburuak(){
+   liburuak_list.forEach(liburua =>{
+      if (liburua.irakurrita){
+         alert(liburua.titulua+ " "+ liburua.autorea)
+      }
+   } )
 }
+
+
 
 //libruuak sartzen joan, librur berribat sartunahi?bai ez, ez ba da irakurritako liburak konprobtau, bai jarraitiu eskatzen
