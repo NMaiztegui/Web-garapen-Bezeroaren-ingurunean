8bed35208e591b742c11b79fb884a35b052799ca
NMaiztegui <naroamaiztegui@gmail.com>
2024-10-01 10:30:20 +0200

    irudia ezabatezen begiartu

--- Erronak1/IRUDIAK AGERTZEN/JS/irudiak.js
+++ Erronak1/IRUDIAK AGERTZEN/JS/irudiak.js
@@ -1,23 +1,55 @@
 window.onload=function(){
     document.getElementById('click').addEventListener('click',addimg,false)
-   let irudia=[
+    irudia=[
         'saludo1.jpg',
         'saludo2.jpg',
         'saludo3.jpg',
         'saludo4.jpg'
     ]
-    let kont=0
-   let addedImg=[]
-
-   function addimg(){
-    kont++
-
-    let img= document.createElement('img')
-    img.src = 'img/'+irudia[kont]
-
-    document.getElementById('irudiak').appendChild(img)
-
-    addedImg[kont]=irudia[kont]
-    
-    }
+    kont=0
+    ezabatutakoIrudiak=[]
 }
+function addimg(){
+    
+       if (kont==4){
+        alert ('Ezin dira irudi gehiago atera')
+       }else{
+        let irudi
+        // comprobar si hay algo en la carpeta de imagenes borradas, significa se se  ha borrado algo
+            if (ezabatutakoIrudiak.lenght>0){
+                //si hay algo en la lista asignar a irudi el valor de la ultima imagen
+                irudi = ezabatutakoIrudiak.pop() // removes the last element from an aaray
+            }
+            else{// si no hay nada en esa carpeta meter la primera imagen
+                 irudi= document.createElement('img')
+                irudi.src = 'img/'+ irudia[kont]   
+                kont++
+            }
+            //listener para eliminar imagen del DOM al hacer doble click
+            irudi.addEventListener('dblclick',ezabatuIrudia)
+
+            
+            let irudiakSecction= document.getElementById('irudiak')
+            irudiakSecction.appendChild(irudi)
+         
+           
+        }
+
+}      
+function ezabatuIrudia(event){
+    let irudi= event.target; // obtener el elemento de DOM que ha sido clikado
+     //borra el elemento de DOM que ha sido clicado
+     irudi.remove();
+    
+     // Añadir la imagen eliminada al array ezabatutakoIrudiak
+     ezabatutakoIrudiak.push(irudi);
+     
+     // Actualizar el contador de imágenes
+     kont--;
+}
+    
+       
+     
+    
+
+
