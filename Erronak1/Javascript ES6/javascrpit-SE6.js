fbfe6cfd83e3edc74aa00b2ef0da5b05e8d36158
NMaiztegui <naroamaiztegui@gmail.com>
2024-10-03 11:55:56 +0200

    javascript-ES6

--- Erronak1/Javascript ES6/javascrpit-SE6.js
+++ Erronak1/Javascript ES6/javascrpit-SE6.js
@@ -7,33 +7,34 @@ class Estudiante{
     }
 }
     // Añadir un método para saludar
-    Estudiante.prototype.saludar = () =>  console.log('Hola, me llamo ' + this.nombre + ' y tengo ' + this.edad + ' años.');
+    Estudiante.prototype.saludar = () =>  console.log(`Hola, me llamo ${nombre}  y tengo   ${edad}  años.`);
     
     // Añadir un método para listar las asignaturas
     Estudiante.prototype.listarAsignaturas =() =>
-    console.log('Mis asignaturas son: ' + this.asignaturas.join(', '));
+    console.log(`Mis asignaturas son: ${asignaturas.join(', ')}`);
     
     // Crear un nuevo estudiante
     
     let estudiante1 = new Estudiante('Ana', 20, 'Matemáticas', 'Historia', 'Literatura');
    
-    // Destructuring manual para extraer nombre y edad
-    var nombre = estudiante1.nombre;
-    var edad = estudiante1.edad;
+ // Destructuring para extraer nombre y edad
+    const { nombre, edad, asignaturas } = estudiante1;
+
     // Mostrar datos del estudiante
-    console.log('Nombre: ' + nombre);
-    console.log('Edad: ' + edad);
+    console.log(`Nombre: ${nombre} `);
+    console.log(`Edad:  ${edad} `);
     // Mostrar saludo y asignaturas
     estudiante1.saludar();
     estudiante1.listarAsignaturas();
     // Función en ES5 para sumar notas
-    function calcularPromedio() {
-    var suma = 0;
-    for (var i = 0; i < arguments.length; i++) {
-    suma += arguments[i];
+    const calcularPromedio= (...numeros) =>{
+        let suma = 0;
+        for (let i = 0; i < numeros.length; i++) {
+        suma += numeros[i];
+        }
+        return suma / numeros.length;
     }
-    return suma / arguments.length;
-    }
-    // Calcular promedio de notas
+    
+    // Calcular promedio de notas templates literal
     var promedio = calcularPromedio(85, 90, 78, 92);
-    console.log('El promedio es: ' + promedio);
+    console.log(`El promedio es: ${promedio}`);
