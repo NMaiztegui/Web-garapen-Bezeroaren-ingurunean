da9eefa284a24d9e0e93710bb166298c50c02c0f
NMaiztegui <naroamaiztegui@gmail.com>
2024-10-09 17:02:28 +0200

    formulario sumit y clear

--- Ariketa_gehigarriak_ikasleentzat/Ariketa gehiagrriak-1/form.js
+++ Ariketa_gehigarriak_ikasleentzat/Ariketa gehiagrriak-1/form.js
@@ -1,5 +1,37 @@
 $(document).ready(function(){
-    $('#formulario').on('submit', function(e){
-        
-    })
+
+   $('#formulario').submit(function (e) { 
+      e.preventDefault();
+      $(this).addClass('was-validated');
+         if (!this.checkValidity()) {
+            e.stopPropagation();
+         }
+         else
+         {  
+            alerta();
+         }
+         
+         
+      });
+      $('#clear').click(function(){
+        $('#formulario')[0].reset(); // Resetea los campos del formulario
+        $('.form-check-input').prop('checked', false); // Desmarca los checkboxes
+        console.log("Formulario limpiado"); // Mensaje en la consola
+      })
+
 });
+   function alerta(){
+    let izena=$('#izenaForm').val();
+    let abizena=$('#abizenaForm').val();
+
+    let aukeratutakoAnimaliak= []
+
+     // Selecciona todos los checkboxes que están marcados
+     $('.form-check-input:checked').each(function() {
+        // Agrega el valor del checkbox marcado al array
+        aukeratutakoAnimaliak.push($(this).val());
+    });
+
+    let email= $('#emailForm').val();
+    alert('Hauek dira zuk sartutako datuak:'+izena+' '+ abizena+' '+aukeratutakoAnimaliak.join(', ')+' '+email)
+   }
