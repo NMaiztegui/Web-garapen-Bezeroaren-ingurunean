cb60ff0e59e5ee7df5878c123ed5fb3b987452e9
Naroa Maiztegui <naroamaiztegui@gmail.com>
2024-11-19 10:57:32 +0100

    start angular nire-osagaia

---
+++ Angular/nireApp/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NireOsagaiaComponent } from './nire-osagaia/nire-osagaia.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NireOsagaiaComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
