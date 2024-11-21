8aaf5cf2b9675d426019c7bf5905a664bed0af1d
Naroa Maiztegui <naroamaiztegui@gmail.com>
2024-11-21 12:38:50 +0100

    FIX LIKES BUTTON ISSUE

--- Angular/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
+++ Angular/nireApp/src/app/nire-osagaia/nire-osagaia.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AnyARecord } from 'node:dns';
 
 @Component({
   selector: 'app-nire-osagaia',
@@ -13,13 +14,17 @@ export class NireOsagaiaComponent {
     irudia:'/assets/caraMujer.jpg'
     };
     langileak = [
-      { izena: "Pepe", abizena: "Perez", generoa: "gizona", soldata: 1000 },
-      { izena: "Sara", abizena: "Ruiz", generoa: "emakumea", soldata: 1000 },
-      { izena: "Paul", abizena: "Elorza", generoa: "gizona", soldata: 1500 },
-      { izena: "Raul", abizena: "Txakartegi", generoa: "gizona", soldata: 2000 }
+      { izena: "Pepe", abizena: "Perez", generoa: "gizona", soldata: 1000, likes:0 },
+      { izena: "Sara", abizena: "Ruiz", generoa: "emakumea", soldata: 1000, likes:0 },
+      { izena: "Paul", abizena: "Elorza", generoa: "gizona", soldata: 1500, likes:0 },
+      { izena: "Raul", abizena: "Txakartegi", generoa: "gizona", soldata: 2000, likes:0 }
       ];
   Title="Langileen zerrenda";
   getTitle() {
     return this.Title;
+  };
+  gehituLikes(langi:any):void{
+    console.log('el boton ha sido presionado');
+    langi.likes++;
   }
 }
